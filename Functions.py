8b70ad477687fd5b092acd58c5f2438682dfde1c
Sebastián Galindez Tapia <sebgal2008@gmail.com>
2018-11-24 20:59:26 +0100

    store

--- Functions.py
+++ Functions.py
@@ -14,3 +14,15 @@ def sellBitcoin(x):
     info = requests.post('http://lauzhack.sqpub.ch', data=data)
 
 
+def streamData():
+    r = requests.get("http://lauzhack.sqpub.ch/prices", stream=True)
+
+    for chunk in r.iter_content(chunk_size=1024):
+        t = 0
+        for x in chunk:
+            if x <= ' ':
+                break
+            t = t + 1
+        print(chunk[t, len(chunk) - 1])
+
+
