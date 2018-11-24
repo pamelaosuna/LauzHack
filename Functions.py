52b44dad0963aabaec6de1d39a39ffcea5a1731c
Sebastián Galindez Tapia <sebgal2008@gmail.com>
2018-11-24 20:06:49 +0100

    starting

---
+++ Functions.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def buyBitcoin(x):
+    x = str(x)
+    data = 'BUY' + x + 'BTC jmf784hkuhkufsd'
+    info = requests.post('http://lauzhack.sqpub.ch', data=data)
+    print(info)
+
+
+def sellBitcoin(x):
+    x = str(x)
+    data = 'SELL' + x + 'BTC jmf784hkuhkufsd'
+    info = requests.post('http://lauzhack.sqpub.ch', data=data)
+
+
