3b63b1345f1d7fbe2cd42acd82678bc48adf2e0d
Manuel-Mockus <36791572+Manuel-Mockus@users.noreply.github.com>
2018-11-25 08:14:28 +0100

    Add files via upload

--- Functions.py
+++ Functions.py
@@ -1,16 +1,15 @@
-import json
 import requests
-import urllib
 import re
+import json
+import urllib
 
-T = 50
-
+T = 30
 
 def findBuy(L):
-    """this function finds the minumum point, which tells us
-    we should buy"""
-    # L: list of 30 elements
-    return L.index(min(L))
+        """this function finds the minumum point, which tells us
+        we should buy"""
+        # L: list of 30 elements
+        return L.index(min(L))
 
 
 def findSell(L):
@@ -19,20 +18,17 @@ def findSell(L):
 
 
 def buyBitcoin(x):
-    x = float(x)
     x = str(x)
     data = 'BUY ' + x + ' BTC jmf784hkuhkufsd'
     info = requests.post('http://lauzhack.sqpub.ch', data=data)
-    print(data)
-
+    print("BUYYY")
 
 
 def sellBitcoin(x):
-    x = float(x)
     x = str(x)
     data = 'SELL ' + x + ' BTC jmf784hkuhkufsd'
     info = requests.post('http://lauzhack.sqpub.ch', data=data)
-    print(data)
+    print("SELLL")
 
 
 def streamData():
@@ -40,34 +36,38 @@ def streamData():
 
     for chunk in r.iter_content(chunk_size=1024):
         t = 0
-        chunk = str(chunk)
         for x in chunk:
             if x <= ' ':
                 break
             t = t + 1
-        i = t + 1
-        for i in range(len(chunk)):
-            if chunk[i] > '9' or chunk[i] < '0' or chunk[i] != '.':
-                break
-            i = i + 1
-        val = float(chunk[t + 1:i])
-        print(val)
-
-
-def choose(L, L_L):
-    var = float("inf")
-    closest = 0
-    i=0
-    for i in range(len(L_L)-1):
-        s = 0
-        for k in range(T):
-            s += abs(L[k] - L_L[i][k])
-        if (s < var):
-            closest = i
-    return i + 1
+        print(chunk[t, len(chunk) - 1])
 
 
 def calculate_Q():
     url = urllib.request.urlopen("http://lauzhack.sqpub.ch/teams")
     data = json.loads(url.read().decode())
     return data[4]["XBT"], data[4]["cash"]
+
+        
+def popLowest(L_L,A):
+    i = A.index(min(A))
+    print(i)
+    L_L.pop(2*i)
+    L_L.pop(2*i)
+    A.pop(i)
+    A.append(0)
+
+def choose(L, L_L):
+    var = float("inf")
+    closest = 0
+    i=0
+    for i in range(int(len(L_L)/2)):
+        s = 0
+        for k in range(T):
+            s += abs(L[k] - L_L[2*i][k])
+        if (s < var):
+            closest = i
+            var = s
+    return 2*i + 1
+    
+    
--- Main.py
+++ Main.py
@@ -3,8 +3,9 @@ import re
 import json
 import urllib
 import Functions
-T = 60
+T = 30
 L_L = []
+A = [0]*100
 L = []
 L_P = []
 S = []
@@ -17,11 +18,12 @@ Sell = 61
 f = open('Data.txt')
 # use readline() to read the first line 
 line = f.readline()
-while line:
+n = 0
+while (line and n<200):
     m = re.search('Z (.+?)\n', line)
     try:
-        i = float(m.group(1))
-        L.append(i)
+        val = float(m.group(1))
+        L.append(val)
         line = f.readline()
         if L.__len__() == T:
             k=[]
@@ -29,14 +31,16 @@ while line:
                 k.append(p - L[0])
             L_L.append(k)
             L = []
-            i = 0
+            n = n+1
     except:
         line = f.readline()
 
 f.close()
 
 XBT, cash = Functions.calculate_Q()
-Q = 2
+QBuy = 2
+QSell = 2
+print(A)
 r = requests.get('http://lauzhack.sqpub.ch/prices', stream=True)
 for chunk in r.iter_content(chunk_size=1024):
     current = chunk
@@ -44,68 +48,41 @@ for chunk in r.iter_content(chunk_size=1024):
     text = chunk.decode("utf-8")
     m = re.search('Z (.+?)\n', text)
     val = float(m.group(1))
-    print(Buy)
-    print(i)
+    print(val)
+    lowIndex = 2
+    print(A[int(lowIndex/2)])
     L.append(val)
     if i == Buy:
-        Functions.buyBitcoin(Q)
+        Functions.buyBitcoin(min(QBuy*(1+4*A[int(lowIndex/2)]),XBT))
+        print(A[int(lowIndex/2)])
+        oldTotal = cash + XBT*val
         XBT, cash = Functions.calculate_Q()
-        Q = (cash / 10) / val
+        total = cash + XBT*val
+        delta = total - oldTotal / oldTotal
+        print(total)
+        QBuy = (cash / 10) / val
     if i == Sell:
-        Functions.sellBitcoin(Q)
+        Functions.sellBitcoin(max(QSell*(1+4*A[int(lowIndex/2)]),cash/val))
+        print(A[int(lowIndex/2)])
+        oldTotal = cash + XBT*val
         XBT, cash = Functions.calculate_Q()
-        Q = (cash / 10) / val
+        total = cash + XBT*val
+        delta = total-oldTotal / oldTotal
+        print(total)
+        XBT,cash = Functions.calculate_Q()
+        QSell = XBT / 10
     i = i + 1
     if L.__len__() == T:
         k = []
         for p in L:
             k.append(p - L[0])
-        L_P = L_L[Functions.choose(L, L_L)]
+        lowIndex = Functions.choose(L, L_L)
+        
+        L_P = L_L[lowIndex]
         L_L.append(k)
+        if(len(L_L) == 202):
+            Functions.popLowest(L_L,A)
         Buy = Functions.findBuy(L_P)
         Sell = Functions.findSell(L_P)
         L = []
         i = 0
-
-
-    def findBuy(L):
-        """this function finds the minumum point, which tells us
-        we should buy"""
-        # L: list of 30 elements
-        return L.index(min(L))
-
-
-def findSell(L):
-    """ finds the maximum point, where we should sell"""
-    return L.index(max(L))
-
-
-def buyBitcoin(x):
-    x = str(x)
-    data = 'BUY' + x + 'BTC jmf784hkuhkufsd'
-    info = requests.post('http://lauzhack.sqpub.ch', data=data)
-    print(info)
-
-
-def sellBitcoin(x):
-    x = str(x)
-    data = 'SELL' + x + 'BTC jmf784hkuhkufsd'
-    info = requests.post('http://lauzhack.sqpub.ch', data=data)
-
-
-def streamData():
-    r = requests.get("http://lauzhack.sqpub.ch/prices", stream=True)
-
-    for chunk in r.iter_content(chunk_size=1024):
-        t = 0
-        for x in chunk:
-            if x <= ' ':
-                break
-            t = t + 1
-        print(chunk[t, len(chunk) - 1])
-
-
-def calculate_Q():
-    url = urllib.request.urlopen("http://lauzhack.sqpub.ch/teams")
-    data = json.loads(url.read().decode())
-    return data[4]["XBT"], data[4]["cash"]
